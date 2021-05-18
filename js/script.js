7f5d43e65461141e0e162a8bb39f00d091df01e0
Emmanuel2525 <72115386+Emmanuel2525@users.noreply.github.com>
2021-05-18 18:30:56 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const slides = document.querySelectorAll(".slideshot-li");
+const left = document.querySelector(".left")
+const right = document.querySelector(".right")
+const slides = document.querySelectorAll(".slideshot-li")
 const slider = document.querySelector(".slideshot")
 let indice = 0;
 
-right.addEventListener("click",goRight);
-left.addEventListener("click",goLeft);
+right.addEventListener("click",goRight)
+left.addEventListener("click",goLeft)
 slider.addEventListener("mouseover",stop)
 slider.addEventListener("mouseout",reset)
 
 function goRight(){
-    slides[indice].style.display ="none";
-    indice++;
+    slides[indice].style.display ="none"
+    indice++
     if (indice>slides.length-1){
         indice = 0;
     }
-    slides[indice].style.animation = "come 1.5s forwards";
-    slides[indice].style.display ="block";
+    slides[indice].style.animation ="come 1.5s forwards"
+    slides[indice].style.display ="block"
 }
 
 function goLeft(){
-    slides[indice].style.display = "none";
+    slides[indice].style.display = "none"
 
     indice--;
     if (indice < 0){
-        indice = slides.length-1;
+        indice = slides.length-1
     }
     
-    slides[indice].style.animation = "gone 1.5s forwards";
-    slides[indice].style.display = "block";
+    slides[indice].style.animation = "gone 1.5s forwards"
+    slides[indice].style.display = "block"
 }
 
 
-var intervalo = setInterval(goRight,4000);
+var intervalo = setInterval(goRight,4000)
 
 function stop(){
     clearInterval(intervalo)
